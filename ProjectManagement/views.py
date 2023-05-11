62dfb3bc01e4d60dfa33f9e49ca3c89c781adbde
Kirui-brian <brinkiru@gmail.com>
2023-05-11 23:27:05 +0300

    update to views.py file

--- ProjectManagement/views.py
+++ ProjectManagement/views.py
@@ -5,3 +5,12 @@ from .serializers import ProjectSerializer
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        instance.delete()
